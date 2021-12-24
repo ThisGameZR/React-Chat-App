a8cb185698f2b8427dfce274075a72ba4a5d4f78
ThisGameZR <timezazare@gmail.com>
2021-12-25 03:11:00 +0700

    Setup basic components and Authen

---
+++ client/src/components/FullScreenCard.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+type FullScreenCardProps = {
+  children: ReactNode;
+};
+
+export function FullScreenCard({ children }: FullScreenCardProps) {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-black">
+      <div className="max-w-md w-full">{children}</div>
+    </div>
+  );
+}
+
+FullScreenCard.Body = function ({ children }: FullScreenCardProps) {
+  return <div className="shadow bg-[#101214]] p-6 rounded-lg">{children}</div>;
+};
+
+FullScreenCard.BelowCard = function ({ children }: FullScreenCardProps) {
+  return <div className="mt-1 justify-center flex gap-3">{children}</div>;
+};
